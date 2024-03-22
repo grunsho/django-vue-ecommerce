3d23fb02624857dfc0646e34e9e963893c97a0b8
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-22 04:10:24 -0300

    fixed location of images

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -40,18 +40,18 @@ class Product(models.Model):
 
     def get_image(self):
         if self.image:
-            return 'http://127.0.0.1:8000' + self.image.url
+            return 'https://grunsho.pythonanywhere.com' + self.image.url
         return ''
     
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'http://127.0.0.1:8000' + self.thumbnail.url
+            return 'https://grunsho.pythonanywhere.com' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
                 
-                return 'http://127.0.0.1:8000' + self.thumbnail.url
+                return 'https://grunsho.pythonanywhere.com' + self.thumbnail.url
             else:
                 return ''
     def make_thumbnail(self, image, size=(300, 200)):
