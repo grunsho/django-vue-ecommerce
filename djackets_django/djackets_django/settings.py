a32a03ac146ab3cc84c8e491bee2d4d5b51193dc
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-22 03:34:49 -0300

    postgres database

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -97,9 +97,17 @@ WSGI_APPLICATION = 'djackets_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'oufv5Vezs1XY',
+        'HOST': 'ep-lively-feather-a455dhg5-pooler.us-east-1.aws.neon.tech',
+        'PORT': '',
     }
 }
 
