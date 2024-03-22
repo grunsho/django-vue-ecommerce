adea268dfbe512c811af0b87a819f65d041ecf88
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-22 02:54:40 -0300

    added whitenoise

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -62,6 +62,7 @@ CORS_ORIGIN_WHITELIST = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
