62a22d0909e78aba1e8e172e2ec921b728d74d73
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-22 02:50:07 -0300

    fixed mimetypes

--- djackets_django/djackets_django/settings.py
+++ djackets_django/djackets_django/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import mimetypes
 
 mimetypes.add_type("text/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
