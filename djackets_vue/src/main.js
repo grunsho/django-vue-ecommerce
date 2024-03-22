e903d44853a04097d169817b31d69e15274d2b13
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-22 01:25:01 -0300

    production directory

--- djackets_vue/src/main.js
+++ djackets_vue/src/main.js
@@ -5,5 +5,5 @@ import store from './store'
 import axios from 'axios'
 
 // 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://grunsho.pythonanywhere.com'
 createApp(App).use(store).use(router, axios).mount('#app')
